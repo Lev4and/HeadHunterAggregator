d7f55af5b3fb494545fcd5bce637ab540b814e61
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-08-18 23:26:19 +0500

    Created authorization and resources API base on ASP.NET Core 6 Web API.

---
+++ HeadHunter.ResourceWebApplication/Program.cs
@@ -0,0 +1,37 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+    loggerConfiguration.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/api/error/unauthorized";
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+        builder.WithOrigins("http://localhost:8080", "http://194-67-67-175.cloudvps.regruhosting.ru").AllowAnyMethod()
+                .AllowAnyHeader().AllowCredentials());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.ToString());
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+app.Run();
