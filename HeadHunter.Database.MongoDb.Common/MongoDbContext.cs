073c584fe4584e2a333bdb7d8a72b4c0185cc93d
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-09-01 21:10:43 +0500

    Created project for buisness logic for work MongoDB.

---
+++ HeadHunter.Database.MongoDb.Common/MongoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+
+namespace HeadHunter.Database.MongoDb.Common
+{
+    public class MongoDbContext
+    {
+        private readonly MongoClient _client;
+
+        public MongoDbContext()
+        {
+            _client = new MongoClient("mongodb://sa:sa@194-67-67-175.cloudvps.regruhosting.ru:27017/?authMechanism=DEFAULT");
+        }
+
+        public IMongoDatabase GetDatabase(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            return _client.GetDatabase(name);
+        }
+    }
+}
