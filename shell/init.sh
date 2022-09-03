f2eba7ceb0d348d3b7f59bb43af6676bbaebd414
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-09-03 22:25:12 +0500

    Update docker-compose.yml file. Improved script initililazing docker.

--- shell/init.sh
+++ shell/init.sh
@@ -10,5 +10,10 @@ git reset --hard origin/master
 rm .env
 cp .env.dist .env
 export $(egrep -v '^#' .env | xargs -0)
+mkdir postgres-data
+mkdir portainer-data
+mkdir mongodb-data
 docker swarm init
 docker stack deploy -c docker-compose.yml headhunter --prune --with-registry-auth
+#docker service ls
+#docker stack rm headhunter 
