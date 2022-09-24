9c63a285f54b1afd7ac7c9a7fd13e9537956cefb
Lev4and <54065747+Lev4and@users.noreply.github.com>
2022-09-24 22:39:17 +0500

    Reverted default value for 'dateTo' and 'dateFrom' variables.

--- HeadHunter.Importer/VacanciesImporter.cs
+++ HeadHunter.Importer/VacanciesImporter.cs
@@ -17,8 +17,8 @@ namespace HeadHunter.Importer
             _logger = logger;
             _context = context;
 
-            _dateTo = DateTime.UtcNow.AddDays(-7).AddMinutes(5);
-            _dateFrom = DateTime.UtcNow.AddDays(-7);
+            _dateTo = DateTime.UtcNow;
+            _dateFrom = DateTime.UtcNow.AddMinutes(-5);
         }
 
         public async IAsyncEnumerable<Vacancy> GetVacanciesAsync()
@@ -35,7 +35,7 @@ namespace HeadHunter.Importer
                 _dateTo = _dateTo.AddMinutes(5);
                 _dateFrom = _dateFrom.AddMinutes(5);
 
-                //if (_dateTo > DateTime.UtcNow) await Task.Delay(_dateTo - DateTime.UtcNow);
+                if (_dateTo > DateTime.UtcNow) await Task.Delay(_dateTo - DateTime.UtcNow);
             }
         }
 
