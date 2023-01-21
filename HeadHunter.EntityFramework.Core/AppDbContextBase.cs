63b13f922332112f285b720541505213450444c9
Lev4and <54065747+Lev4and@users.noreply.github.com>
2023-01-21 14:52:52 +0500

    Created 'HeadHunter.EntityFramework.Core' project.

---
+++ HeadHunter.EntityFramework.Core/AppDbContextBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeadHunter.EntityFramework.Core
+{
+    public abstract class AppDbContextBase : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+    {
+        public AppDbContextBase(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ HeadHunter.EntityFramework.Core/Extensions/RelationshipExtensions.cs
@@ -0,0 +1,29 @@
+﻿using HeadHunter.Core.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq.Expressions;
+
+namespace HeadHunter.EntityFramework.Core.Extensions
+{
+    public static class RelationshipExtensions
+    {
+        public static void OneToOne<TEntity, TRelatedEntity>(this EntityTypeBuilder<TEntity> builder, 
+            Expression<Func<TEntity, TRelatedEntity?>> field, Expression<Func<TRelatedEntity, TEntity?>> foreignField, 
+            Expression<Func<TRelatedEntity, object?>> foreignKey, bool isRequired = true) 
+            where TEntity : EntityBase, IAggregateRoot
+            where TRelatedEntity : EntityBase, IAggregateRoot
+        {
+            builder.HasOne(field).WithOne(foreignField).HasForeignKey(foreignKey).IsRequired(isRequired);
+        }
+
+        public static void OneToMany<TEntity, TRelatedEntity>(this EntityTypeBuilder<TEntity> builder, 
+            Expression<Func<TEntity, IEnumerable<TRelatedEntity>?>> arrayField, 
+            Expression<Func<TRelatedEntity, TEntity?>> foreignField, 
+            Expression<Func<TRelatedEntity, object?>> foreignKey, 
+            bool isRequired = true) 
+            where TEntity : EntityBase, IAggregateRoot 
+            where TRelatedEntity : EntityBase, IAggregateRoot
+        {
+            builder.HasMany(arrayField).WithOne(foreignField).HasForeignKey(foreignKey).IsRequired(isRequired);
+        }
+    }
+}
